40115bde1ed2dce5c5dfc93d55bc0e487c4c2faa
Hernando De Avila <hdeavilaigabem@gmail.com>
2023-11-13 21:57:44 -0500

    La cague borre la parte importante

--- Resistencia_Materiales/RDM.py
+++ Resistencia_Materiales/RDM.py
@@ -32,6 +32,7 @@ class objeto():
 #SU: Resistencia última en Pascales.
 #epsilon: Deformación unitaria 
 #n: Factor de seguridad.
+#"force": is an indicator that a property is a force. 
 """
     def __init__(self) -> None:
         """Este el el objeto principal donde están guardadas las funciones 
@@ -43,19 +44,19 @@ class objeto():
         self.pro["Contorno_f"]=np.array([])
         self.pro["Contorno_m"]=np.array([])
     def asignar(self,dict):
-        """_Asing a dictionary of variables to the main object _
+        """_summary_
 
         Args:
-            dict (_dict_): _Dictionary with al the variables used in the example._
+            dict (_type_): _description_
         """
         self.pro=dict
     def add(self,name,propertie,printed=False):
-        """_Add new variable to the main object _
+        """_summary_
 
         Args:
-            name (_string_): _Name of the new variable example: Fuerza1, Torq1, stress0 _
-            propertie (_float_): _Numerical value of the new variable_
-            printed (bool, optional): _Determine if you want to print (True) or not (False) the information about the main object_. Defaults to False.
+            name (_type_): _description_
+            propertie (_type_): _description_
+            printed (bool, optional): _description_. Defaults to False.
         """
         if type(name)==type(np.array([0])):
             c=0
@@ -63,18 +64,53 @@ class objeto():
                 self.pro[i]=propertie[c]
                 c+=1
         else:
-            self.pro[name]=propertie
+            self.pro[name]=[propertie,unity]
         if (printed==True):
             print("++++++++++++++++++++++")
             print("------------!Información!-------------")
-            print("Se agrega la propiedad "+str(propertie))
+            print("Se agrega la propiedad "+str(propertie)+" "+str(unity))
             print("Al diccionario del objeto 'RDM' con la key: "+name)
+    def fill(self,name,unity,printed=False):
+        """Esta función agrupa de forma automatica todas las variables que compartan la misma unidad(unity)
+        Por ejemplo agrupa todas las fuerzas o todos los momentos que experimenta un cuerpo rígido.
+
+        Args:
+            name (_string_): _Nombre de la variable en la que será guardada la sumatoria normalmente como estandas
+            se escoge Contorno_f y Contorno_m como nombre de variables para guardar la sumatoria de momento y la 
+            sumatoria de fuerzas._
+            unity (_string_): _Es la unidad que comparte las diferentes variables dentro del diccionario, puede ser
+            fuerza, momento, inercia, velocidad, Área, entre otros.._
+            printed (bool, optional): _Herramienta que te permite ver si las asginaciones se realizaron de manera
+            correcta. _. Defaults to False.
+        """
+        if self.pro.get(name) == None:
+            self.pro[name]=np.array([])
+        hstack=True
+        for key in self.pro.keys():
+            if len(self.pro[key])>1 and key!=name:
+                print("Banderaaa *************")
+                print(key)
+                print(self.pro[key][1])
+                if self.pro[key][1]==unity:
+                    if printed==True:
+                        print("Se identifica "+str(key)+" como elemento con unidad de "+str(unity))
+                    if hstack==True:
+                        self.pro[name]=np.hstack((self.pro[name],self.pro[key][0])) 
+                        hstack=False
+                    else:
+                        self.pro[name]=np.vstack((self.pro[name],self.pro[key][0])) 
+                    if printed==True:
+                        print("Se agrega "+str(self.pro[key][0])+" a "+str(name))
+        self.pro[name]=[self.pro[name],unity]
+        print("Llenado exitoso")
+
+     
     def delta(self,name="Delt",P="P",L="L",A="A",E="E"):
         """Function that allows me to calculate the displacement in millimeters, caused by a central axial force on an object.
 
         Args:
-            name (str, optional): Name of the contained object who you wish to calculate displacement.. Defaults to "Delt".
-            P (str, optional): Axial force on the object surface.  . Defaults to "P".
+            name (str, optional): Name of the contained object who you wish to calculate displacement. Defaults to "Delt".
+            P (str, optional):  Axial force on the object surface. Defaults to "P".
             L (str, optional): Longitude of the interest object. Defaults to "L".
             A (str, optional): Area of the interest object . Defaults to "A".
             E (str, optional): Young’s module of the interest object material. Defaults to "E".
@@ -130,15 +166,55 @@ class objeto():
             print("Calculo automatico de esfuerzo a fallado, verificar manualmente")
         else:
             print("Calculo de esfuerzo automatico sin problemas")
-    def sum(self,Contorno_f="Contorno_f",Contorno_m="Contorno_m",print=False):
-        Sf=np.array([0,0])
-        for i in self.pro[Contorno_f]:
-            Sf=Sf+i[1:]
-        Sm=np.array([0,0])
-        for j in self.pro[Contorno_m]:
-            Sm=Sm+j[1:]
+    def solve(self,Obj1,Obj2,Sf="P",Delt="Delt"):
+        """_summary_
+
+        Args:
+            Obj1 (_type_): _description_
+            Obj2 (_type_): _description_
+            Sf (str, optional): _description_. Defaults to "P".
+            Delt (str, optional): _description_. Defaults to "Delt".
+        """
+        R=sp.solve([self.pro[Sf],self.pro[Delt]],dict=True)
+        Obj1.pro["P"]=R[0][Obj1.pro["P"]]
+        Obj2.pro["P"]=R[0][Obj2.pro["P"]]
+    def solve1(self,Ecu,Incg):
+        """_summary_
+
+        Args:
+            Ecu (_type_): _description_
+            Incg (_type_): _description_
+        """
+        R=sp.solve(Ecu,dict=True)
+        for i in Incg:
+            self.pro[i]=R[0][self.pro[i]]
+    def sum(self,Contorno_f="Contorno_f",Contorno_m="Contorno_m",printed=False):
+        """_summary_
+
+        Args:
+            Contorno_f (str, optional): _description_. Defaults to "Contorno_f".
+            Contorno_m (str, optional): _description_. Defaults to "Contorno_m".
+            printed (bool, optional): _description_. Defaults to False.
+        """
+        init=True
+        for i in self.pro[Contorno_f][0]:
+            if init==True:
+                Sf=np.zeros_like(i[0:])
+                init=False
+            Sf=Sf+i
+        #-------------------------------------------------    
+        init=True
+        for j in self.pro[Contorno_m][0]:
+            if init==True:
+                try:
+                    Sm=np.zeros(len(j))
+                except:
+                    Sm=self.pro[Contorno_m][0]
+                    break
+                init=False
+            Sm=Sm+j
         self.add("S",[Sf,Sm])
-        if print==True:
+        if printed==True:
             print("+++++++++++++++++++++++++++++++++++++++++++++")
             print("Sistemas de ecuación de fuerza:\n "+str(Sf))
             print("Sistemas de ecuación de momento:\n "+str(Sm))
@@ -146,6 +222,15 @@ class objeto():
             print("+++++++++++++++++++++++++++++++++++++++++++++")
 
     def Unitarian(V,VM):
+        """_summary_
+
+        Args:
+            V (_type_): _description_
+            VM (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         Lambda=np.array([])
         c=0
         for i in VM:
@@ -163,23 +248,55 @@ class objeto():
             c+=1
             Lambda=Lambda.transpose()
             return Lambda
+    def cross_product(self,distance,force,Resultingmomentum="MR",unity="momentum"):
+        """_summary_
 
+        Args:
+            distance (_type_): _description_
+            force (_type_): _description_
+            Resultingmomentum (str, optional): _description_. Defaults to "MR".
+            unity (str, optional): _description_. Defaults to "momentum".
+        """
+        if len(distance)==len(force):
+            self.pro[Resultingmomentum]=np.array([])
+            print("Se realizara el producto cruz")
+            hstack=True
+            for i, j in zip(distance,force):
+                if hstack==True:
+                    print(i)
+                    print(type(i[0]))
+                    print(type(j[0]))
+                    print(j)
+                    print(np.cross(i,j))
+                    self.pro[Resultingmomentum]=np.hstack( (self.pro[Resultingmomentum],np.cross(i,j)) )
+            else:
+                    self.pro[Resultingmomentum]=np.vstack((self.pro[Resultingmomentum],np.cross(i,j)) )
+        else:
+            print("Los vectores de distancia y fuerza no son compatibles")
+            print(str(len(distance))+" Para distancia")
+            print(str(len(force))+ " Para fuerza")
+            print("Por favor verifique")
+        self.pro[Resultingmomentum]=[self.pro[Resultingmomentum],unity]
+        
 
     def momento(self,Nnodo,S="S",Contorno="Contorno_f"):
-        """
-        ## Momento de una fuerza en un nodo
-        ### Esta función permite calcular el momento de una fuerza asignando 
-        ### un nodo específico que debe estar en el listado de nodos.
+        """_summary_
 
-        1. Nnodo: Valor del nodo donde se calculara el momento de una fuera.
-
-        2. Por defecto la Key para las ecuaciones de sumatoria de fuerza es "S"
-            pero se puede cambiar bajo consideración 
-
-        3. Se recomienda realizar la suma de fuerza en unop de los apoyos
-            para eliminar una incognita del sistemas
+        Args:
+            Nnodo (_type_): _description_
+            S (str, optional): _description_. Defaults to "S".
+            Contorno (str, optional): _description_. Defaults to "Contorno_f".
         """
         def vector_dist(Nnodo,M):
+            """_summary_
+
+            Args:
+                Nnodo (_type_): _description_
+                M (_type_): _description_
+
+            Returns:
+                _type_: _description_
+            """
             index=np.where(M[:,0]==Nnodo)
             try:
                 A=M[index[0],1:]
@@ -191,8 +308,19 @@ class objeto():
                 exit()
             return B
         def vector_mag(V):
+            """_summary_
+
+            Args:
+                V (_type_): _description_
+
+            Returns:
+                _type_: _description_
+            """
             VC=V**2
-            VM=np.sqrt(VC[:,0]+VC[:,1])
+            try:
+                VM=np.sqrt(VC[:,0]+VC[:,1])
+            except:
+                VM=np.sqrt(VC[:,0]+VC[:,1]+VC[:,2])
             return VM
 
 
@@ -223,6 +351,8 @@ class objeto():
         self.add("Sm",Det)
 
     def build(self):
+        """_summary_
+        """
         [Sf,Sm]=self.pro["S"]
         R=sp.solve(np.append(Sf,np.array([Sm[0]+Sm[1]+self.pro["Sm"]]) ),dict=True)   
         k1,k2=R[0].keys()
@@ -245,6 +375,15 @@ class objeto():
 
 
     def Area(self,tipo,D="D",AREA="A",t="t",anch="Anch"):
+        """_summary_
+
+        Args:
+            tipo (_type_): _description_
+            D (str, optional): _description_. Defaults to "D".
+            AREA (str, optional): _description_. Defaults to "A".
+            t (str, optional): _description_. Defaults to "t".
+            anch (str, optional): _description_. Defaults to "Anch".
+        """
         if tipo=="c":
             self.pro[AREA]=A(self.pro[D])
             print("Área "+AREA+" asignada como: "+str(self.pro[AREA])+" en "+D)
@@ -254,6 +393,8 @@ class objeto():
             else:
                 print("Calcule el Área manualmente")
     def Info(self):
+        """_summary_
+        """
         lista=self.pro.keys()
         c=0
         print("------Informa de Propiedades-----------")
@@ -262,26 +403,24 @@ class objeto():
             c+=1
         print("------  Fin   -------------")
     def distance(self,A,B,L="L"):
-        """
-        ## Función distancia.
-        Esta función permite calcular la distancia desde un puntos existente en el objeto hasta otro:
-        A: String que contiene el nombre del primer Vector de tipo numpy array, 
-            contiene las coordenadas cartecianas del primer punto.
-        B: String que contiene el nombre del segundo Vector de tipo numpy array, contiene las 
-            coordenadas cartecianas del segundo punto.
-        L: Nombre en string de la variable donde deseas almacenar la longitud del vector.
+        """_This function allows to calculate distances from a given point in the object to another point._
+
+        Args:
+            A (_type_): _description_
+            B (_type_): _description_
+            L (str, optional): _description_. Defaults to "L".
         """
         V=self.pro[B]-self.pro[A]
         LV=np.linalg.norm(V)
         self.add(L,LV)
         return
     def deformacion(self,L="L",Lp="Lp",epsilon="epsilon"):
-        """"
-        ## Calculo de deformación unitaria.
-        Determina la deformación de un elemento introduciendo como variable de entrada.
-        L: string-->Nombre de la longitud inicial. expl{"L","L0","L1",...}
-        Lp: string--->Nombre de la longitud final. expl{"Lf","Lp","L2",...}
-        epsilon: string---> es el lugar por defecto, donde se guarda la variable calculada.
+        """_Calculates the unitarian deformation_
+
+        Args:
+            L (str, optional): _description_. Defaults to "L".
+            Lp (str, optional): _description_. Defaults to "Lp".
+            epsilon (str, optional): _description_. Defaults to "epsilon".
         """
         try:
             e=(self.pro[Lp]-self.pro[L])/self.pro[L]
@@ -296,27 +435,24 @@ class objeto():
         self.add(epsilon,e)
         return
     def J(self,Jindex="J",d="d"):
-        """
-        ##Esta función permite cálcular la inercia polar de un elemento circular masiso
-        Como parametro de entrada tiene el diametro de la sección circular, su valor por defecto
-        es "d" pero puede ser modificado:
-        Entradas:
-        * Jindex="J" : Variable que almacena la inercia polar.
-        * d="d": Diametro esperado del eje.
+        """_Allow to calculate the polar inertia of a circular geometry _
+
+        Args:
+            Jindex (str, optional): _description_. Defaults to "J".
+            d (str, optional): _description_. Defaults to "d".
         """
         J=0.5*np.pi*((self.pro[d]*0.5)**4)
         self.add(Jindex,J)
         return
     def tau(self,tau="tau",T="T",d="d",c="c",J="J"):
-        """
-        ## El codigo permite calcular el esfuerzo cortante Tau conociendo el diametro de la figura,
-        el torque y el lugar donde se quiere medir.
+        """_Calculates the shear stress of a cylindrical geometry_
 
-        * tau: Variable que almacena el esfuerzo cortante, su nombre por defecto es "tau"
-        * T: Torque de entrada del eje, su nombre por defecto es "T"
-        * d: Dinametro del eje, su nombre por defecto es "d"
-        * c: Radio de interes para el analisis de esfuerzo cortante.
-        * J: Momento polar de inercia de un circulo, su valor por defecto es "J"
+        Args:
+            tau (str, optional): _description_. Defaults to "tau".
+            T (str, optional): _description_. Defaults to "T".
+            d (str, optional): _description_. Defaults to "d".
+            c (str, optional): _description_. Defaults to "c".
+            J (str, optional): _description_. Defaults to "J".
         """
         Tor=self.pro[T]
         c=self.pro[c]
@@ -327,21 +463,35 @@ class objeto():
 
 #Función de calculo de área.
 def A(D):
-    """Function that calculates the transversal area of a cylindrical object.
+    """_summary_
 
     Args:
-        D (_float_): Diameter of the circle. 
+        D (_type_): _description_
 
     Returns:
-        _float_: area  
+        _type_: _description_
     """
     return (np.pi*(D**2))/4
 #Función de diámetro de circulo
 def D(A):
+    """_summary_
+
+    Args:
+        A (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return sp.sqrt(4*A/np.pi)
 
 #Constructor de resultados:
 def Respuesta(Enunciados,Respuestas):
+    """_summary_
+
+    Args:
+        Enunciados (_type_): _description_
+        Respuestas (_type_): _description_
+    """
     c=0
     print("*********************")
     for i in Enunciados:
